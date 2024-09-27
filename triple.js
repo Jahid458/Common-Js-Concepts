486da2df11fa1f5c1be8f9f213a90ce144d9b1e2
Jahid458 <jahidul898@outlook.com>
2024-09-28 02:26:20 +0600

    type value comparison

---
+++ triple.js
@@ -0,0 +1,20 @@
+
+//alalways use ===
+//equal comparion does not work for an non-primitive 
+
+
+// const first = '0 '; 
+// const second =  false; value and type
+
+const first = []; 
+const second =  [];
+
+if(first === second){
+  console.log('Values are equal')
+}else{
+  console.log('Values are not equal')
+}
+
+//type coercion 
+// type conversion
+//type casting
