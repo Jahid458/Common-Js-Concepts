a0bdfbe0c40169d1392f97b681de5ff918ed5802
Jahid458 <jahidul898@outlook.com>
2024-09-28 11:13:47 +0600

    arguments

---
+++ argumernts.js
@@ -0,0 +1,12 @@
+
+
+//Arguments array liek object
+function sum(a,b,c){
+  const args = [...arguments];
+  // console.log(args)
+  const result = a + b + c ; 
+  return result;
+}
+
+const total = sum(45,89, 12, 45, 98, 56);
+console.log(sum.length)
